fc3e6996cb3c2122cdbf2c1350e52d013bfd1927
sealdev <frederikvendelbo4@gmail.com>
2026-04-16 23:16:22 +0200

    Added more tests

--- tests/test-warena-api.c
+++ tests/test-warena-api.c
@@ -8,34 +8,45 @@
 #include "./../src/wdebug.h"
 
 static int test_alloc_creation() {
-  alloc_t* mockalloc = new_allocator(4);
-
-  return (mockalloc->alloc_ctx->size == (4 * PAGE_SIZE));
+  return (new_allocator(1) != 0) ? 1 : 0;
 }
 
-/* 
- * Im silly so I cannot think of a way to
- * meassure an allocations success other
- * than it not segfaulting
- */
+static int test_null_alloc_creation() {
+  return (new_allocator(0) == 0) ? 1 : 0;
+}
+
+static int test_alloc_destroy() {
+  alloc_t* allocator = new_allocator(1);
+  
+  return (allocator->destroy(allocator->alloc_ctx) != 0 ? 1 : 0);
+}
+
+static int test_null_alloc_destroy() {
+  alloc_t* allocator = new_allocator(1);
+
+  return (allocator->destroy(0) == 0 ? 1 : 0);
+}
+
 static int test_allocation() {
   alloc_t* allocator = new_allocator(4);
 
   size_t used_mem = allocator->alloc_ctx->allocated;
   size_t aval_mem = allocator->alloc_ctx->size - used_mem;
   
-  char* test = allocator->alloc(allocator->alloc_ctx, aval_mem);
-  memset(test, 101, aval_mem - 1);
-  long_string[aval_mem] = '\0';
+  char* teststr = allocator->alloc(allocator->alloc_ctx, aval_mem);
+  memset(teststr, 101, aval_mem - 1);
+  teststr[aval_mem] = '\0';
   
   allocator->destroy(allocator->alloc_ctx);
 
   return 1;
 }
 
-/*
- * Same idea here! No SEGFAULT No STRESS
- */ 
+static int test_null_allocation() {
+  alloc_t* allocator = new_allocator(4);
+
+  return (allocator->alloc(allocator->alloc_ctx, 0) == 0 ? 1 : 0);
+}
 
 static int test_multi_allocator() {
   size_t page_num  = 12;
@@ -50,7 +61,7 @@ static int test_multi_allocator() {
     char* teststr = allocator->alloc(allocator->alloc_ctx, aval_mem);
     
     memset(teststr, 101, aval_mem - 1);
-    test[aval_mem] = '\0';
+    teststr[aval_mem] = '\0';
 
     allocator->destroy(allocator->alloc_ctx);
   }
@@ -58,12 +69,84 @@ static int test_multi_allocator() {
   return 1;
 }
 
+static int test_isheap_start() {
+  alloc_t* allocator = new_allocator(1);
+  
+  int result = allocator->is_heap_ptr(allocator->alloc_ctx,
+                                 (uintptr_t) allocator->alloc_ctx);
+
+  allocator->destroy(allocator->alloc_ctx);
+
+  return result;
+}
+
+static int test_isheap_middle() {
+  alloc_t* allocator = new_allocator(1);
+
+  uintptr_t alloc_mid = (uintptr_t) allocator + 
+                        (allocator->alloc_ctx->size / 2);
+  
+  int result = allocator->is_heap_ptr(allocator->alloc_ctx, alloc_mid);
+
+  allocator->destroy(allocator->alloc_ctx);
+
+  return result;
+}
+
+static int test_isheap_end() {
+
+  alloc_t* allocator = new_allocator(1);
+
+  uintptr_t alloc_end = (uintptr_t) allocator + allocator->alloc_ctx->size - 1;
+    
+  int result = allocator->is_heap_ptr(allocator->alloc_ctx, alloc_end);
+
+  allocator->destroy(allocator->alloc_ctx);
+
+  return result;
+}
+
+
+static int test_is_not_heap_high() {
+  alloc_t* allocator = new_allocator(1);
+
+  uintptr_t high_ptr = (uintptr_t) allocator->alloc_ctx +
+                      (allocator->alloc_ctx->size + 1);
+
+  int result = !(allocator->is_heap_ptr(allocator->alloc_ctx, high_ptr));
+
+  allocator->destroy(allocator->alloc_ctx);
+
+  return result;
+}
+
+static int test_is_not_heap_low() {
+  alloc_t* allocator = new_allocator(1);
+
+  uintptr_t low_ptr = (uintptr_t) allocator->alloc_ctx - 1;
+
+  int result = !(allocator->is_heap_ptr(allocator->alloc_ctx, low_ptr));
+
+  allocator->destroy(allocator->alloc_ctx);
+
+  return result;
+}
+
 extern void warena_api_test_runner() {
   char test_success = 1;
 
-  test_success =  test_alloc_creation();
-  test_success =      test_allocation();
-  test_success = test_multi_allocator();
+  test_success =      test_alloc_creation();
+  test_success = test_null_alloc_creation();
+  test_success =       test_alloc_destroy();
+  test_success =  test_null_alloc_destroy();
+  test_success =          test_allocation();
+  test_success =     test_null_allocation();
+  test_success =     test_multi_allocator();
+  test_success =        test_isheap_start();
+  test_success =       test_isheap_middle();
+  test_success =          test_isheap_end();
+  test_success =     test_is_not_heap_low();
+  test_success =    test_is_not_heap_high();
 
   char* test_msg = (test_success) ? "PASS" : "FAIL";
   printf("[%s] %s \n", test_msg, __FILE__);
