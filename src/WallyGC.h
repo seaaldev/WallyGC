445b907c6565e0d5ec571405aae44b84ca3f64b8
sealdev <frederikvendelbo4@gmail.com>
2026-04-24 03:54:42 +0200

    Fixes in wlist.h
    - Fixed some faulty logic with the wgc_list_link_valid check. Where we
    unintentionally had swapped two checks around:
    
    "next->prev == next && prev->next == prev" - WRONG
    
    "next->prev == prev && prev->next == next" - RIGHT

--- src/WallyGC.h
+++ src/WallyGC.h
@@ -3,8 +3,10 @@
 
 #include <stddef.h>
 
-extern void* wgc_malloc(size_t size);
+extern 
+void *wgc_malloc(size_t size);
 
-extern void wgc_free(void* ptr);
+extern 
+void wgc_free(void* ptr);
 
 #endif
--- src/wlist.h
+++ src/wlist.h
@@ -7,8 +7,8 @@
 /* ------------------------ PUBLIC INTERACE ------------------------ */
 
 struct list_head {
-  struct list_head* next;
-  struct list_head* prev;
+  struct list_head *next;
+  struct list_head *prev;
 };
 
 // Initialize a struct list_head to point to itself
@@ -16,27 +16,27 @@ struct list_head {
 
 // Definition of a &struct list_head with initialization values 
 #define LIST_HEAD(name) \
-  struct list_head name = LIST_HEAD_INIT
+  struct list_head name = LIST_HEAD_INIT(name)
 
 // Insert a new entry after the specified head
 static inline
-void list_add(struct list_head* new, struct list_head* head);
+void list_add(struct list_head *new, struct list_head *head);
 
-// Initialize & insert a new entry before the specified head
+// Insert a new entry before the specified head
 static inline
-void list_add_tail(struct list_head* new, struct list_head* head);
+void list_add_tail(struct list_head *new, struct list_head *head);
 
 // Delete entry from list
 static inline
-void list_del(struct list_head* entry);
+void list_del(struct list_head *entry);
 
 // Move an existing entry after the specified head
 static inline
-void list_move(struct list_head* entry, struct list_head* head);
+void list_move(struct list_head *entry, struct list_head *head);
 
 // Move an existing entry to before the specified head
 static inline 
-void list_move_tail(struct list_head* entry, struct list_head* head);
+void list_move_tail(struct list_head *entry, struct list_head *head);
 
 
 /* 
@@ -49,17 +49,17 @@ void list_move_tail(struct list_head* entry, struct list_head* head);
 #define POISON 0xDEADDEADDEADDEAD
 
 static inline
-bool wgc_list_link_valid(struct list_head* new,
-                         struct list_head* next,
-                         struct list_head* prev)
+bool wgc_list_link_valid(struct list_head *new,
+                         struct list_head *next,
+                         struct list_head *prev)
 {
-  return (next->prev == next && prev->next == prev && new != next && new !=prev);
+  return (next->prev == prev && prev->next == next && new != next && new !=prev);
 }
 
 static inline
-void wgc_list_link(struct list_head* new,
-                   struct list_head* next,
-                   struct list_head* prev)
+void wgc_list_link(struct list_head *new,
+                   struct list_head *next,
+                   struct list_head *prev)
 {
   if (!wgc_list_link_valid(new, next, prev))
     return;
@@ -72,79 +72,72 @@ void wgc_list_link(struct list_head* new,
 }
 
 static inline
-bool wgc_list_del_entry_valid(struct list_head* entry)
+bool wgc_list_del_entry_valid(struct list_head *entry)
 {
-  struct list_head* next = entry->next;
-  struct list_head* prev = entry->prev;
+  struct list_head *next = entry->next;
+  struct list_head *prev = entry->prev;
 
   return (prev->next == entry && next->prev == entry);
 }
 
 static inline
-void wgc_list_del_link(struct list_head* next, struct list_head* prev)
+void wgc_list_del_link(struct list_head *next, struct list_head *prev)
 {
   next->prev = prev;
   prev->next = next;
 }
 
 static inline
-void wgc_list_del_entry(struct list_head* entry) 
+void wgc_list_del_entry(struct list_head *entry) 
 {
   if (!wgc_list_del_entry_valid(entry))
     return;
 
   wgc_list_del_link(entry->next, entry->prev);
-  entry->next = POISON;
-  entry->prev = POISON;
-}
-
-static inline
-void wgc_list_move(struct list_head* entry,
-                   struct list_head* next,
-                   struct list_head* prev)
-{
-  wgc_list_del_link(entry->next, entry->prev);
-  wgc_list_link(entry, next, prev); 
+  entry->next = (struct list_head *) POISON;
+  entry->prev = (struct list_head *) POISON;
 }
 
 /* 
  * ------------------------------------------------------------------ 
- * PUBLIC ROUTINES BELOW:
+ * NORMAL ROUTINES BELOW:
  * ------------------------------------------------------------------ 
  */
 
 static inline
-void list_add(struct list_head* new,
-              struct list_head* head)
+void list_add(struct list_head *new,
+              struct list_head *head)
 {
   wgc_list_link(new, head->next, head);
 }
 
 static inline
-void list_add_tail(struct list_head* new,
-                   struct list_head* head)
+void list_add_tail(struct list_head *new,
+                   struct list_head *head)
 {
   wgc_list_link(new, head, head->prev);
 }
 
 static inline
-void list_del(struct list_head* entry) 
+void list_del(struct list_head *entry) 
 {
-  wgc_list_del_entry
+  wgc_list_del_entry(entry);
 }
 
 static inline
-void list_move(struct list_head* entry,
-               struct list_head* head)
+void list_move(struct list_head *entry,
+               struct list_head *head)
 {
-  wgc_list_move(entry, head->next, head);
+  wgc_list_del_entry(entry);
+  wgc_list_link(entry, head->next, head);
 }
 
 static inline 
-void list_move_tail(struct list_head* entry,
-                    struct list_head* head) 
+void list_move_tail(struct list_head *entry,
+                    struct list_head *head) 
 {
-  wgc_list_move(entry, head, head->prev);
+  wgc_list_del_entry(entry);
+  wgc_list_link(entry, head, head->prev);
 }
 
 #endif // ! WLIST_H
